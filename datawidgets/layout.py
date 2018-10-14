b88d0ad06e80d5a7a5dd764a64e1e9c1f9334c32
Ryan Grout <ryan@ryangrout.org>
2018-10-13 20:41:11 -0500

    First working version.

--- datawidgets/layout.py
+++ datawidgets/layout.py
@@ -1,8 +1,10 @@
-import ipywidgets as ipyw
-import collections
 import inspect
-
-from .widget_types import WIDGET_MAP
+import collections
+import ipywidgets as ipyw
+from collections.abc import Sequence
+from collections import OrderedDict
+from operator import attrgetter
+from functools import wraps
 
 MISSING = set()
 try:
@@ -16,6 +18,21 @@ try:
     MISSING.add(attr.NOTHING)
 except ImportError:
     pass
+
+WIDGET_MAP = {
+    int: (ipyw.IntSlider, 'value'),
+    float: (ipyw.FloatSlider, 'value'),
+    str: (ipyw.Text, 'value'),
+    bool: (ipyw.Checkbox, 'value'),
+    list: (ipyw.Dropdown, 'options'),
+    tuple: (ipyw.Dropdown, 'options'),
+    set: (ipyw.Dropdown, 'options')
+}
+
+ATTRS_FIELDS = '__attrs_attrs__'
+DATACLASS_FIELDS = '__dataclass_fields__'
+
+WIDGET = '__widget__'
 
 ATTRS_FIELDS = '__attrs_attrs__'
 DATACLASS_FIELDS = '__dataclass_fields__'
@@ -45,7 +62,6 @@ def _fields(instance):
             return attr.fields(instance.__class__)
     raise TypeError("Not an instance of dataclass or attrs")
 
-
 def _observe_handlers(instance):
     """Return a map of attributes and handlers
 
@@ -58,30 +74,37 @@ def _observe_handlers(instance):
             # skip magic methods
             continue
 
-        if inspect.ismethod(obj):
-            observees = getattr(obj, '__observe', [])
-            for observee in observees:
-                handlers[observee].append(obj)
+        if inspect.isfunction(obj):
+            print(name, obj)
+            observees = getattr(obj, '_observes', {})
+            for observee, kwargs in observees.items():
+                handlers[observee].append((-1, obj, kwargs))
+
     return handlers
 
-def layout(instance, public_attributes_only=True):
+def create_widgets(instance, public_attributes_only=True):
     fields = _fields(instance)
 
     handlers = _observe_handlers(instance)
+    print(handlers)
     widgets = []
+    __widgets = OrderedDict()
     for field in fields:
         if public_attributes_only and field.name.startswith('_'):
             continue
         kwargs = {'description': field.name}
-        kwargs.update(field.metadata.get('__widget__', {}))
+        kwargs.update(field.metadata.get('__widget', {}))
 
         try:
-            widget_class = WIDGET_MAP[field.type]
+            _type = field.type
+            widget_class, dattr = WIDGET_MAP[_type]
         except KeyError:
             if field.default not in MISSING:
-                widget_class = WIDGET_MAP[type(field.default)]
+                _type = type(field.default)
+                widget_class, dattr = WIDGET_MAP[_type]
             elif field.default_factory not in MISSING:
-                widget_class = WIDGET_MAP[type(field.default_factory())]
+                _type = type(field.default_factory())
+                widget_class, dattr = WIDGET_MAP[_type]
             else:
                 raise TypeError("Unable to find widget type for {}".format(field.name))
 
@@ -89,21 +112,47 @@ def layout(instance, public_attributes_only=True):
             kwargs['value'] = field.default
 
         widget = widget_class(**kwargs)
-        for observer in handlers[field.name]:
-            widget.observe(observer)
-            widget.observe(setter(instance, field.name), names='value')
-        widgets.append(widget)
+        for _t, observer, kwargs in handlers[field.name]:
+            widget.observe(observer, names='value')
 
+        widgets.append(widget)
+        __widgets[field.name] = widget
+
+    instance.__widgets = __widgets
     return widgets
 
 
-def setter(instance, attribute):
-    def _set(event):
-        setattr(instance, attribute, event['new'])
-    return _set
-
-def observe(*attributes):
-    def _observe(method):
-        method.__observe = attributes
-        return method
+def _observer(instance, attribute, name):
+    def _observe(event):
+        setattr(instance, attribute, event[name])
     return _observe
+
+
+class Sync(object):
+    def __init__(self, method, observes, updates):
+        self.method = method
+        self.observes = observes
+        self.updates = updates
+
+    def _sync_to_widgets(self, instance):
+        _widgets = getattr(instance, '__widgets', {})
+        if not _widgets:
+            return
+        for attribute, trait in self.updates.items():
+            setattr(_widgets[attribute], trait, getattr(instance, attribute))
+
+    def __get__(self, instance, owner):
+        @wraps(self.method)
+        def method(*args, **kwargs):
+            rv = self.method(instance, *args, **kwargs)
+            if self.updates:
+                self._sync_to_widgets(instance)
+            return rv
+        method._observes = self.observes
+        method._updates = self.updates
+        return method
+
+def sync(observes=None, updates=None):
+    def _sync(method):
+        return Sync(method, observes or {}, updates or {})
+    return _sync
